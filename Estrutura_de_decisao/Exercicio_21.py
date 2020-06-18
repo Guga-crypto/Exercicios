98dfe54126cb30b4f5038659526e9d2d53631240
Guga-crypto <luizfmonteiro@outlookcom>
2020-06-18 02:23:14 -0300

    resolvido
    resolvido com algumas mudanças

--- Estrutura_de_decisao/Exercicio_21.py
+++ Estrutura_de_decisao/Exercicio_21.py
@@ -4,26 +4,22 @@ print('\nValor minimo de saque: R$ 10,00')
 print('Valor máximo de saque: R$ 600,00')
 saque = int(input('\nDigite o valor a ser sacado: '))
 
-nota_1 = 1
-nota_2 = 5
-nota_3 = 10
-nota_4 = 50
-nota_5 = 100
+cem = int(saque / 100)
+saque = saque % 100
 
-if 10 <= saque < 50:
-    unidade = saque % 10
-    unidade = int(unidade)
-    if 1 <= unidade < 5:
-        print(f'{unidade} notas de 1')
-    elif unidade == 5:
-        print('Uma nota de 5.')
-    elif 5 < unidade <= 9
-        
-    aux = saque % 10
-    dezena = (saque - aux) / 10
-    dezena = int(dezena)
-    print(f'{dezena} notas de 10')
+cinquenta = int(saque / 50)
+saque = saque % 50
 
-else:
-    print('erro.')
+dez = int(saque / 10)
+saque = saque % 10
 
+cinco = int(saque / 5)
+saque = saque % 5
+
+um = saque
+
+print('Notas R$100,00 = ', cem)
+print('Notas R$ 50,00 = ', cinquenta)
+print('Notas R$ 10,00 = ', dez)
+print('Notas R$  5,00 = ', cinco)
+print('Notas R$  1,00 = ', um)
